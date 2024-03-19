875a7b4c16994d2a3ea06e54888418853488288d
rosembackpedrob <rosembackpedrob@gmail.com>
2024-03-19 15:59:43 -0300

    Changelog 0.14 - Movimentação Gamepad e Change Rooms
    Novidades:
    - Teleporte entre salas refatorado e modular;
    - Movimentação usando InputActions, ou seja, funciona em múltiplos
    tipos de controle.
    
    Próximos Objetivos:
    - Controle da UI pelo gamepad;
    - Interação entre puzzle, itens e recompensa (como porta abrindo).
    Após a tarefa de estruturação geral da lógica das classes em UML;
    - Interação com puzzles pelo controle;
    
    !Avisos:
    - Novidades Implementadas somente na cena "PedroScene"
    - Novo script de movimento do jogador "PlayerMovement"
    foi criado para evitar conflito com atual ao introduzir o InputActions.
    Sugestão: copiar o jogador da cena Pedro para cena de testes gerais Sample Scene ou GodoiScene).

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    [SerializeField] string thisRoom;
+    [SerializeField] public string nextRoom;
+    [SerializeField] public Vector3 playerNewPos;
+
+}
