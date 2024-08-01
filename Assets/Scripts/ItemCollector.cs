e92adb3429b180a93c4d2a9d62373054143b1a9c
Randooel <leonardo.alves.barros.1@gmail.com>
2024-08-01 17:25:21 -0300

    changelog 0.7 - WIP Implementação do puzzle 2 (ingredientes)
    - Implementado:
         - Ingredientes
         - Caldeirão
         - Cortina
    - WIP liberação da porta média (para a sala do caldeirão)

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -7,6 +7,8 @@ public class ItemCollector : MonoBehaviour
     public InventoryManager inventoryManager;
     public InventoryUiController inventoryUi;
 
+    public int cucaPzzl2Itens = 0;
+
     private void Start()
     {
         inventoryManager.items.Clear();
@@ -22,5 +24,14 @@ public class ItemCollector : MonoBehaviour
             inventoryUi.UpdateUI();
             Destroy(collision.gameObject);
         }
+
+
+        if (collision.gameObject.name.Contains("batWing") || collision.gameObject.name.Contains("vultureFeather") || collision.gameObject.name.Contains("ounceTooth") || 
+            collision.gameObject.name.Contains("potionVial") || collision.gameObject.name.Contains("spiderLeg"))
+        {
+            cucaPzzl2Itens++;
+            Debug.Log(cucaPzzl2Itens);
+        }
+
     }
 }
