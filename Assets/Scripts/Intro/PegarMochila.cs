69bc43185132de96ba284066fdbcd4085931292d
Randooel <leonardo.alves.barros.1@gmail.com>
2024-09-08 15:31:07 -0300

    0.27 - Finalização da Iteração
    - Névoas grossas das outras áreas somem quando o jogador pega a mochila
    - Saída para cena da Loira do Banheiro adicionada
    - Cena da Loira adicionada na build (necessário pra troca entre cenas)
    
    PRÓXIMO(S) PASSO(S):
    - Adicionado Y sort nos arbustos da mochila sapo
    - Configurar saída para cena da Matinta
    - Implementação dos assets de arte, som e efeitos visuais (assim que estiverem prontos)

--- Assets/Scripts/Intro/PegarMochila.cs
+++ Assets/Scripts/Intro/PegarMochila.cs
@@ -6,6 +6,8 @@ public class PegarMochila : MonoBehaviour
 {
     public GameObject sapoMochila;
     public GameObject nevoa;
+    public GameObject nevoa2;
+    public GameObject nevoa3;
     void Start()
     {
         sapoMochila.SetActive(false);
@@ -16,6 +18,8 @@ public class PegarMochila : MonoBehaviour
         {
             sapoMochila.SetActive(true);
             nevoa.gameObject.SetActive(false);
+            nevoa2.gameObject.SetActive(false);
+            nevoa3.gameObject.SetActive(false);
             gameObject.SetActive(false);
         }
     }
